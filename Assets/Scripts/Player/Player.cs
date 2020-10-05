e81788a8edcd9a40f75ceb977514683ba01caf2f
AshenBushi <61754110+AshenBushi@users.noreply.github.com>
2020-10-05 12:02:40 +0300

    Add DOTween

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private List<MovingPoint> _movingPoints = new List<MovingPoint>();
+    }
+}
