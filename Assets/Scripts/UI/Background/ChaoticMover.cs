2e8295ba45d6cf669c55b0a9a32e7e49d9c0a6d5
AshenBushi <61754110+AshenBushi@users.noreply.github.com>
2020-10-22 15:26:37 +0300

    Background Animation

---
+++ Assets/Scripts/UI/Background/ChaoticMover.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChaoticMover : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [SerializeField] private float _lapTime;
+    
+    private Tween _moveTween;
+    private Vector3 _startPoint;
+    private Vector3 _pointToMove;
+    
+    private void Start()
+    {
+        _startPoint = transform.position;
+        StartMove();
+    }
+
+    private void StartMove()
+    {
+        GeneratePointToMove();
+        _moveTween = transform.DOMove(_pointToMove, Distance() * _lapTime).SetEase(Ease.Linear);
+        _moveTween.OnComplete(StartMove);
+    }
+
+    private void GeneratePointToMove()
+    {
+        _pointToMove = new Vector3(_startPoint.x + Random.Range(-_radius, _radius), _startPoint.y + Random.Range(-_radius, _radius));
+    }
+
+    private float Distance()
+    {
+        var position = transform.position;
+        return Mathf.Sqrt(Mathf.Pow(_pointToMove.x - position.x, 2) + Mathf.Pow(_pointToMove.y - position.y, 2));
+    }
+}
